95378089cc00891e90cc8d220bc786650942fff4
Rogerio <rgwar@hotmail.com>
2018-05-24 22:49:32 -0300

    teste de ncomando Git

--- class/usuario.php
+++ class/usuario.php
@@ -2,77 +2,98 @@
 
 class Usuario{
 
-private $idusuario;
-private $delogin;
-private $dessenha;
-private $dtcadastro;
+	private $idusuario;
+	private $deslogin;
+	private $dessenha;
+	private $dtcadastro;
 
-/////////////////////////////////////////
-public function getIdusuario(){
-	return $this->idusuario;
-}
+//________________getes e seters________________//
+	public function getIdusuario(){
+		return $this->idusuario;        //sem argumentos retorna o valor da propriedade na instância atual
+	}
 
-public function setIdusuario($value){
-$this->idusuario = $value;
-}
+	public function setIdusuario($value){
+		$this->idusuario = $value;        //atribui o valor informado
+	}
 
-//////////////////////////////////////////
-public function getDeslogin(){
-	return $this->deslogin;
-}
+//___________________________________________________
+	public function getDeslogin(){
+		return $this->deslogin;
+	}
 
-public function setDeslogin($value){
-$this->deslogin = $value;
-}
+	public function setDeslogin($value){
+		$this->deslogin = $value;
+	}
 
-///////////////////////////////////////////
+//____________________________________________________
 
-public function getDessenha(){
-	return $this->dessenha;
-}
+	public function getDessenha(){
+		return $this->dessenha;
+	}
 
-public function setDessenha($value){
-$this->dessenha = $value;
-}
+	public function setDessenha($value){
+		$this->dessenha = $value;
+	}
 
-///////////////////////////////////////////
+//______________________________________________________
 
-public function getDtcadastro(){
-	return $this->dtcadastro;
-}
+	public function getDtcadastro(){
+		return $this->dtcadastro;
+	}
 
-public function setDtcadastro($value){
-$this->dtcadastro = $value;
-}
+	public function setDtcadastro($value){
+		$this->dtcadastro = $value;
+	}
+//_______________________________________________________
 
-   //carrega os dados do banco para o objeto 
-public function loadById($id){
+ //carrega os dados do banco para o objeto, alimenta, com os dados do banco, os dados que caracterizam e fazem o próprio filho dessa Classe, instanciado.
+	public function loadById($id){
 
-$sql = new Sql();//cria uma instancia de SQL que extends PDO com seus métodos
+	$sql = new Sql();//cria uma instancia de SQL que extends PDO com seus métodos
 
-$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
-":ID"=>$id
+//em uma variável q é um SQL_filho atribui o q é desejado pelo código...no parametro da function
+
+$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(    //array[0]
+	":ID"=>$id
 ));
-if (count($results) > 0){ 
-$row = $results[0];
+if (count($results) > 0){ //poderia ser if(isset($results[0]))   ........se existe
+	$row = $results[0];
 
-$this->setIdusuario($row['idusuario']);
-$this->setDeslogin($row['deslogin']);
-$this->setDessenha($row['dessenha']);
-$this->setDtcadastro(new DateTime($row['dtcadastro']));
-          }
-
+	$this->setIdusuario($row['idusuario']);
+	$this->setDeslogin($row['deslogin']);
+	$this->setDessenha($row['dessenha']);
+	$this->setDtcadastro(new DateTime($row['dtcadastro']));
+} 
 
 }
+/////////////////////////////////////////////////////
+public static function getList(){
+
+	$sql = new SQL();
+
+	return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+}
+
+
+
+
+
+
 
 public function __toString(){
 
-return json_encode(array(
-"idusuario"=>$this->getIdusuario(),
-"deslogin"=>$this->getDeslogin(),
-"dessenha"=>$this->getDessenha(),
-"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
-));
+	return json_encode(array(
+
+		"idusuario"=>$this->getIdusuario(),
+		"deslogin"=>$this->getDeslogin(),
+		"dessenha"=>$this->getDessenha(),
+		"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+	));
+
+
+
+}
 
 
 }
@@ -80,9 +101,4 @@ return json_encode(array(
 
 
 
-}
-
-
-
-
- ?>
+?>
--- index.php
+++ index.php
@@ -7,12 +7,28 @@ require_once("config.php");
 // $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 // echo json_encode($usuarios);
-
-$root = new Usuario();
+/////////////////////////////////////////////////////////////////////////////////
+//$root = new Usuario();
 
 //nesse loadById já existe uma instância de SQL na classe usuario
-$root->loadById(4);
+//$root->loadById(8);
 
-echo $root;
+//echo $root;
 
- ?>
+///////////////////////////////////////////////////////////////////////////////////
+
+//$teste = array("fruta=>figo", "legume=>tomate");
+//echo json_encode($teste);
+/* pelo que eu pude perceber ao desdobrar este código
+ao solicitar duas vezes o comando json_encode 
+a função não retorna corretamente como em uma única chamada
+pelo menos no chrome*/
+
+$lista = Usuario::getList();      //chamada de método estático não precisa de instância da classe
+
+echo json_encode($lista);
+
+
+
+
+?>
